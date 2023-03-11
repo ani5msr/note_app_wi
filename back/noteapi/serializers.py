9793b6ab90ebba23dd55671828d78c9354db3e5b
Anirudh Mishra <anirudh5mar@gmail.com>
2023-03-11 13:12:42 +0530

    added comments

--- back/noteapi/serializers.py
+++ back/noteapi/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from notes.models import Note
 class NoteSerializer(serializers.ModelSerializer):
     created = serializers.ReadOnlyField()
+    #fields are the json objects which we allow to be serialized
     class Meta:
         model = Note
         fields = ['id','title','description','created']
