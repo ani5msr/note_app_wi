816033d39076b6d09df3c18e26aba4cec9c8fb33
Anirudh Mishra <anirudh5mar@gmail.com>
2023-03-11 12:06:11 +0530

    Added react bootstrap navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddNote from './components/add-note';
+import NotesList from './components/notes-list';
+import Login from './components/login';
+import Signup from './components/signup';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Navbar bg="primary" variant="light">
+<div className="container-fluid">
+<Navbar.Brand>Notes App</Navbar.Brand>
+<Nav className="me-auto">
+<Nav.Link href="#home">Home</Nav.Link>
+<Nav.Link href="#link">Link</Nav.Link>
+</Nav>
+</div>
+</Navbar>
+</div>
+);
 }
-
 export default App;
